a1acfc23c8bb43bdee6db118df74d811f6143722
Dario Espinola <despinola91@gmail.com>
2024-06-05 21:59:03 -0300

    Mejora de estructura en clase clique

--- src/negocio/Clique.java
+++ src/negocio/Clique.java
@@ -4,46 +4,30 @@ import java.util.ArrayList;
 
 public class Clique {
 
-	ArrayList<Nodo> _listaNodo;
+	ArrayList<Vertice> _vertices;
 	private int _peso;
 
 	public Clique() {
-		_listaNodo = new ArrayList<Nodo>();
+		_vertices = new ArrayList<Vertice>();
 		this._peso = 0;
 	}
 
-	public void agregarNodoAClique(Nodo nodo, Grafo grafo, Arista arista) {  //falta agregar peso arista?
-		if (!_listaNodo.contains(nodo) && esVecinodeTodos(nodo, grafo)) {
-			_listaNodo.add(nodo);
-			
-		}
-	}
-
-	private boolean esVecinodeTodos(Nodo nodo, Grafo grafo) {
-		boolean ret = true;
-		for (Nodo iteracion : _listaNodo) {
-			ret = ret && grafo.obtenerIndiceConVecinos().get(iteracion.obtenerId()).contains(nodo);
-		}
-		return ret;
-	}
-
-	public Clique cliqueConMasPeso(Clique otra) {
-		if (this._peso > otra.obtenerPeso()) {
-			return this;
-		}
-		return otra;
-	}
-
-	public ArrayList<Nodo> obtenerListaNodo() {
-		return _listaNodo;
+	public ArrayList<Vertice> obtenerVertices() {
+		return _vertices;
 	}
 
 	public int obtenerPeso() {
+		for (Vertice vertice :_vertices) {
+			_peso += vertice.obtenerPeso();
+		}
 		return _peso;
 	}
 
 	public int obtenerGrado() {
-		return this._listaNodo.size();
+		return this._vertices.size();
 	}
 
+    public void agregarVertice(Vertice vertice) {
+        _vertices.add(vertice);
+    }
 }
